a4294cb7fa32aa9ac10c6db7be092c8f7590bbea
Prerana Chaudhari <chaudharipv1991@gmail.com>
2021-10-04 22:14:32 -0400

    Final edit

--- email_on_traffic.py
+++ email_on_traffic.py
@@ -1,10 +1,9 @@
 import requests
 import smtplib
 
+# API Key in a file
 
-# api key
-
-api_file = open('API key for Google maps.txt', 'r')
+api_file = open('..', 'r')
 api_key = api_file.read()
 api_file.close()
 
@@ -21,58 +20,29 @@ seconds = get_request.json()['rows'][0]['elements'][0]['duration']['value']
 print ("Time to reach from home to work is: ", time )
 
 if (seconds > 3600):
-    sender = 'pvcpython11@gmail.com'
-    reciepient = 'pvcpython11@gmail.com'
+    sender = '..'
+    reciepient = '..'
     subject = "Email from Python code for Google Maps API"
-    message = "Hi, the code works!"
-    
+    message = "Hello! The commute time is more than an hour."  
+    password = '..'
+
+    # Format email
     email = 'Subject: {}\n\n{}'.format(subject,message)
-
-    password = 'tishapro11'
-
+    
+    # Create a SMTP session 
     send_email = smtplib.SMTP("smtp.gmail.com",587)
 
+    # Start TLS for security 
     send_email.starttls()
 
+    # Authentication 
     send_email.login(sender,password)
 
+    # Sending the mail 
     send_email.sendmail(sender,reciepient,email)
 
+    # Terminating the session
     send_email.quit()
 
-    print ("Sent email")
-
-# check if travel time is more than 1 hour
-# if (seconds > 3600):
-#     # email constraints
-#     sender = "pvcpython11@gmail.com"    
-#     recipient = "pvcpython11@gmail.com"          
-#     subject = "Sick Day"   
-#     message = "Hi Team,\n\nSorry, but I can't make it into work today."
-    
-#     # format email
-#     email = "Subject: {}\n\n{}".format(subject, message)
-    
-#     # get sender password
-#     # password_file = open("password.txt", "r")
-#     password = 'tishapro11'
-#     # password_file.close()
-    
-#     # creates SMTP session 
-#     s = smtplib.SMTP("smtp.gmail.com", 587) 
-      
-#     # start TLS for security 
-#     s.starttls() 
-      
-#     # authentication 
-#     print (sender, password)
-#     s.login(sender, password) 
-      
-#     # sending the mail 
-#     s.sendmail(sender, recipient, email)
-      
-#     # terminating the session 
-#     s.quit() 
-
-#     # success message
-#     print("\nSuccessfully sent a sick-day email to", recipient, "since the travel time was too long")
+    # Success message
+    print ("Sent email since commute takes more than an hour!")
